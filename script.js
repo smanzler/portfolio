ae488c9b3de9fa59fad2b61ba529de90187aa43b
simanzler <simanzler@gmail.com>
2024-09-05 14:36:12 -0400

    Added style change on scroll for header

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const typewriterElement = document.getElementById("typewriter");
+const header = document.getElementById("header");
+const logo = document.getElementById("logo");
 const phrases = ["Software Developer", "Student", "Hard Worker"];
 let phraseIndex = 0;
 
@@ -10,3 +12,16 @@ function changePhrase() {
 setInterval(changePhrase, 8000);
 
 changePhrase();
+
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) {
+    console.log("scroll");
+    header.style["background-color"] = "#0000004d";
+    logo.style["font-size"] = "3rem";
+  } else {
+    header.style["background-color"] = "transparent";
+    logo.style["font-size"] = "5vw";
+  }
+});
