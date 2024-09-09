0d3132ea58fe513024329e9374982cd547e4fefb
simanzler <simanzler@gmail.com>
2024-09-09 02:40:19 -0400

    Added blur to header and added custom scroll bar

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ const typewriter = document.getElementById("typewriter");
 const header = document.getElementById("header");
 const logo = document.getElementById("logo");
 const headerLogo = document.getElementById("header-logo");
+const progress = document.getElementById("progress-bar");
+
 const phrases = ["Software Developer", "Student", "Hard Worker"];
 let phraseIndex = 0;
+let totalHeight = document.body.scrollHeight - window.innerHeight;
 
 function changePhrase() {
   typewriter.textContent = phrases[phraseIndex];
@@ -18,11 +21,16 @@ changePhrase();
 window.addEventListener("scroll", (e) => {
   console.log(e);
   const scrollPosition = window.scrollY;
+  const progressHeight = (scrollPosition / totalHeight) * 100;
+  console.log(progressHeight);
+  progress.style.height = progressHeight + "%";
 
-  if (scrollPosition > 100) {
+  if (scrollPosition > 50) {
     console.log("scroll");
-    header.style["background-color"] = "#0000001a";
+    header.style.backgroundColor = "#0000001a";
+    header.style.backdropFilter = "blur(5px)";
   } else {
-    header.style["background-color"] = "transparent";
+    header.style.backgroundColor = "transparent";
+    header.style.backdropFilter = "none";
   }
 });
