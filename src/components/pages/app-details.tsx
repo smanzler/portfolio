734fb4eb2fa6984a57806bec8fa439b111c5cad4
Simon Manzler <simanzler@gmail.com>
2025-11-11 11:29:01 -0500

    updated app details to use typography

--- src/components/pages/app-details.tsx
+++ src/components/pages/app-details.tsx
@@ -16,6 +16,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "../ui/accordion";
+import { H1, H3, Lead, P, UL } from "../ui/typography";
 
 const AppDetails = () => {
   const { id } = useParams();
@@ -61,132 +62,127 @@ const AppDetails = () => {
   }
 
   return (
-    <div className="min-h-screen py-20">
-      <h1 className="text-4xl md:text-5xl font-bold mb-8 tracking-tight">
-        {app.title}
-      </h1>
+    <div className="min-h-screen flex flex-col gap-8">
+      <div className="flex flex-row justify-between items-end">
+        <div>
+          <H1>{app.title}</H1>
+          <Lead>{app.description}</Lead>
+        </div>
 
-      <div className="flex flex-col gap-12">
-        {/* Screenshots */}
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-row gap-4 overflow-x-auto">
-            {app.screenshots &&
-              app.screenshots.map((screenshot, index) => (
-                <div
-                  key={index}
-                  className="rounded-xl overflow-hidden border w-1/3 min-w-64"
-                >
-                  <img
-                    src={screenshot}
-                    alt={screenshot}
-                    className="w-full h-auto"
-                  />
-                </div>
-              ))}
-          </div>
+        <div className="rounded-lg overflow-hidden size-16">
+          <img
+            src={app.image}
+            alt={app.title}
+            className="size-full object-cover"
+          />
         </div>
-        {/* Description */}
-        <div className="flex flex-col gap-2">
-          <h2 className="text-2xl font-bold tracking-tight">Description</h2>
-          <p className="text-base text-muted-foreground leading-relaxed">
-            {app.longDescription || app.description}
-          </p>
-        </div>
-        {/* Features */}
-        <div className="flex flex-col gap-2">
-          <h2 className="text-2xl font-bold tracking-tight">Features</h2>
-          <ul className="list-disc list-inside">
-            {app.features &&
-              app.features.map((feature, index) => (
-                <li
-                  key={index}
-                  className="text-base text-muted-foreground leading-relaxed"
-                >
-                  {feature}
-                </li>
-              ))}
-          </ul>
-        </div>
-        {/* Links */}
-        {app.link ||
-          (app.github && (
-            <div className="flex flex-col gap-2">
-              <h2 className="text-2xl font-bold tracking-tight">Links</h2>
-              <ul className="list-disc list-inside">
-                {app.link && (
-                  <li>
-                    <a
-                      href={app.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {app.link}
-                    </a>
-                  </li>
-                )}
-                {app.github && (
-                  <li>
-                    <a
-                      href={app.github}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      GitHub
-                    </a>
-                  </li>
-                )}
-              </ul>
-            </div>
-          ))}
-        {/* Reviews */}
-        {app.reviews && (
-          <div className="flex flex-col gap-2">
-            <h2 className="text-2xl font-bold tracking-tight">Reviews</h2>
-            <div className="flex flex-row gap-4 overflow-x-auto">
-              {app.reviews.map((review, index) => (
-                <Card key={index} className="w-1/3 min-w-64">
-                  <CardHeader>
-                    <CardTitle>{review.name}</CardTitle>
-                    <CardDescription>{review.review}</CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="flex flex-row gap-2">
-                      {Array.from({ length: review.rating }).map((_, index) => (
-                        <Star
-                          key={index}
-                          className="h-4 w-4 text-yellow-500"
-                          fill="currentColor"
-                        />
-                      ))}
-                      {Array.from({ length: 5 - review.rating }).map(
-                        (_, index) => (
-                          <Star key={index} className="h-4 w-4 text-gray-400" />
-                        )
-                      )}
-                    </div>
-                  </CardContent>
-                </Card>
-              ))}
-            </div>
-          </div>
-        )}
-        {/* FAQ */}
-        {app.faq && (
-          <div className="flex flex-col gap-2">
-            <h2 className="text-2xl font-bold tracking-tight">FAQ</h2>
-            <div className="flex flex-col gap-2">
-              <Accordion type="single" collapsible>
-                {app.faq.map((faq, index) => (
-                  <AccordionItem key={index} value={faq.question}>
-                    <AccordionTrigger>{faq.question}</AccordionTrigger>
-                    <AccordionContent>{faq.answer}</AccordionContent>
-                  </AccordionItem>
-                ))}
-              </Accordion>
-            </div>
-          </div>
-        )}
       </div>
+
+      {/* Screenshots */}
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row gap-4 overflow-x-auto">
+          {app.screenshots &&
+            app.screenshots.map((screenshot, index) => (
+              <div
+                key={index}
+                className="rounded-xl overflow-hidden border w-1/3 min-w-64"
+              >
+                <img
+                  src={screenshot}
+                  alt={screenshot}
+                  className="w-full h-auto"
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+      {/* Description */}
+      <div className="flex flex-col gap-2">
+        <H3>Description</H3>
+        <P className="text-base text-muted-foreground leading-relaxed">
+          {app.longDescription || app.description}
+        </P>
+      </div>
+      {/* Features */}
+      <div>
+        <H3>Features</H3>
+        {app.features && <UL items={app.features} />}
+      </div>
+      {/* Links */}
+      {app.link ||
+        (app.github && (
+          <div className="flex flex-col gap-2">
+            <H3>Links</H3>
+            <ul className="list-disc list-inside">
+              {app.link && (
+                <li>
+                  <a href={app.link} target="_blank" rel="noopener noreferrer">
+                    {app.link}
+                  </a>
+                </li>
+              )}
+              {app.github && (
+                <li>
+                  <a
+                    href={app.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GitHub
+                  </a>
+                </li>
+              )}
+            </ul>
+          </div>
+        ))}
+      {/* Reviews */}
+      {app.reviews && (
+        <div className="flex flex-col gap-2">
+          <H3>Reviews</H3>
+          <div className="flex flex-row gap-4 overflow-x-auto">
+            {app.reviews.map((review, index) => (
+              <Card key={index} className="w-1/3 min-w-64">
+                <CardHeader>
+                  <CardTitle>{review.name}</CardTitle>
+                  <CardDescription>{review.review}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex flex-row gap-2">
+                    {Array.from({ length: review.rating }).map((_, index) => (
+                      <Star
+                        key={index}
+                        className="h-4 w-4 text-yellow-500"
+                        fill="currentColor"
+                      />
+                    ))}
+                    {Array.from({ length: 5 - review.rating }).map(
+                      (_, index) => (
+                        <Star key={index} className="h-4 w-4 text-gray-400" />
+                      )
+                    )}
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
+      {/* FAQ */}
+      {app.faq && (
+        <div>
+          <H3>FAQ</H3>
+          <div className="flex flex-col gap-2">
+            <Accordion type="single" collapsible>
+              {app.faq.map((faq, index) => (
+                <AccordionItem key={index} value={faq.question}>
+                  <AccordionTrigger>{faq.question}</AccordionTrigger>
+                  <AccordionContent>{faq.answer}</AccordionContent>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
